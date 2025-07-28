aa221aad55494e66c37b4f87830f13cd39e96e98
xaviachristi <xaviachristi@gmail.com>
2025-07-28 11:21:53 +0100

    Add Event listener instead of onclick attribute

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function saveLead() {
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
     console.log("Button clicked!")
-}
+})
+// cleaner separation of concerns where the html doesn't worry abt the javascript - no "onclick" element in the html.
+
