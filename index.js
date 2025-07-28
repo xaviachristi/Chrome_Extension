479773654ee25a5b8f036d35545aaf89be9105a2
xaviachristi <xaviachristi@gmail.com>
2025-07-28 11:30:47 +0100

    Updates

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let inputBtn = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el") //can use both let and const to declare variables.
+const inputBtn = document.getElementById("input-btn") //const cannot be reassigned
 
 inputBtn.addEventListener("click", function() {
     console.log("Button clicked!")
