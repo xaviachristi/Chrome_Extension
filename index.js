0ff58f3ce58378017f650f01f6ed6cc9745e1efd
xaviachristi <xaviachristi@gmail.com>
2025-07-29 09:25:21 +0100

    Progress;

--- index.js
+++ index.js
@@ -3,26 +3,40 @@ const inputEl = document.getElementById("input-el") //can use both let and const
 const inputBtn = document.getElementById("input-btn") //const cannot be reassigned
 const ulEl = document.getElementById("ul-el")
 const container = document.getElementById("container")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+// console.log(leadsFromLocalStorage)
 
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads) //clear DOM by rendering out the leads
+})
 
 inputBtn.addEventListener("click", function() { // cleaner separation of concerns where the html doesn't worry abt the javascript - no "onclick" element in the html.
     myLeads.push(inputEl.value)
     inputEl.value=""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-    
+    render(myLeads)
+
     // To verify that it works:
-    console.log( localStorage.getItem("myLeads") )
+    // console.log( localStorage.getItem("myLeads") )
 })
 
-function renderLeads() {
+
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li><a target = '_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>" //converts this to a list item - manipulating the DOM - creating HTML elements with JS.
+    for (let i = 0; i < leads.length; i++) {
+        listItems += "<li><a target = '_blank' href='" + leads[i] + "'>" + myLeads[i] + "</a></li>" //converts this to a list item - manipulating the DOM - creating HTML elements with JS.
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         ` //backticks allow formatting on diff lines - just like in the HTML
